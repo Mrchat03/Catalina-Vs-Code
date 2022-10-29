9027f632c4446649661f03763fb8219d2f081ca8
Nath <34311270+Mrchat03@users.noreply.github.com>
2022-10-29 00:17:50 -0400

    Ajout du template pour démarer un projet

---
+++ Template/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c - Laboratoire #: 
+ *
+ * Programme 
+ * 247-465-SH, Hiver 2022
+ *
+ * @author <votre nom>
+ * @version version 0.1
+ */
+#include <stdio.h>
+#include <catalina_cog.h>
+
+void main(int argc, char *argv[])
+{
+   
+    while (1) 
+    {
+        
+    }
+}
